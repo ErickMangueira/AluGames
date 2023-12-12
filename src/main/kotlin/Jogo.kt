459cc191bdfa88763ad395c57f5ab1183875db08
Erick Mangueira <erickmangueira@gmail.com>
2023-12-12 09:12:52 -0300

    Consumindo a api e criando a model jogo

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,13 @@
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+    override fun toString(): String {
+        return "Meu Jogo:\n" +
+        "Titulo:  $titulo \n" +
+        "Capa:   $capa \n" +
+        "Descrição: $descricao "
+    }
+
+
+}
